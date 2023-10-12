f3103979a8edf3f6ddeaf1339d4d4227d80311a6
João Victor Mazetti Borges <jvmazettib@gmail.com>
2023-10-12 16:27:40 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const nameElement = document.querySelector('#name');
+    const usernameElement = document.querySelector('#username');
+    const avatarElement = document.querySelector('#avatar');
+    const reposElement = document.querySelector('#repos');
+    const followersElement = document.querySelector('#followers');
+    const followingElement = document.querySelector('#following');
+
+    fetch('https://api.github.com/users/jvmazetti')
+        .then(function(res){
+            return res.json();
+        })
+        .then(function(json){
+            nameElement.innerText = json.name;
+            usernameElement.innerText = json.login;
+            avatarElement.src = json.avatar_url;
+            followingElement.innerText = json.following;
+            followersElement.innerText = json.followers;
+            reposElement.innerText = json.public_repos;
+        })
+})
